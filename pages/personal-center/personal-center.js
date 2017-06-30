8bb3b9abcab51f9135bbbcb4ef2058f92bac3262
Yifan Zhao <Yifan Zhao>
2017-06-30 16:07:11 +0800

    add login btn

--- pages/personal-center/personal-center.js
+++ pages/personal-center/personal-center.js
@@ -74,5 +74,17 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  login: function() {
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          //发起网络请求
+          console.log(res);
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
+      }
+    });
   }
 })
